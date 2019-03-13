878525452d52cd2d0cc8a3fa1252ba78a28f5933
Leonardo de-Oliveira-Martins (QIB) <Leonardo Martins leomrtns@gmail.com>
2019-03-13 16:04:54 +0000

    template of bioconda recipe

---
+++ conda-recipes/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://bioconda.github.io/troubleshooting.html#zlib-errors
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
+# cd to location of Makefile and source
+mkdir build
+cd build
+# assumes autoreconf was run and thus configure.sh is present 
+$SRC_DIR/super_sptree/configure --prefix=$PREFIX
+make
+make install
