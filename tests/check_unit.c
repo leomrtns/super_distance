3eee1ea79a98756b189b215f81000c26dbc3a45c
Leonardo de-Oliveira-Martins (QIB) <Leonardo Martins leomrtns@gmail.com>
2019-03-20 17:34:05 +0000

    added dummy unit tests, now 'check' is a dependency

---
+++ tests/check_unit.c
@@ -0,0 +1,12 @@
+#include <super_sptree.h> 
+
+#define TEST_SUCCESS 0
+#define TEST_FAILURE 1
+#define TEST_SKIPPED 77
+#define TEST_HARDERROR 99
+
+int
+main (int argc, char **argv)
+{
+  return TEST_SUCCESS; 
+}
